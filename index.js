dd9e90129c8e73148a41de93eb2b02048c1f55db
srkreddydendi <srkreddydendi@gmail.com>
2020-10-07 14:42:35 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 var arr = [];
-var x = "abcd <Tooltip placement='top' content='hello world aurn welcome'>abcd hello worlds ared </Tooltip> xyz asdfsadfsdfsdfsdfdsf <Tooltip placement='top' content='xus asdfsadfdfa'>hello world </Tooltip> asdasd <Tooltip placement='top' content='hello1 world1'>xyx sdfsdfsdfdsf sss</Tooltip>"
-f(x)
-var i = 0;
-function f(x){
- // console.log(x)
-  let start = x.indexOf("<Tooltip")
-  let end = x.indexOf("</Tooltip>")
-  let resp = x.substring(start, end+10);
-  //console.log(resp) 
-  let content = prepareContent(resp)
-  let text = prepateText(resp)
-  arr.push({content:content,text:text })
-  let nextText = x.substr(end+10, x.length)
-  if(nextText.includes("<Tooltip"))
-    f(nextText)
+var x =
+  "<p> abcd <Tooltip placement='top' content='hello world aurn welcome'>abcd hello worlds ared </Tooltip> xyz asdfsadfsdfsdfsdfdsf <Tooltip placement='top' content='xus asdfsadfdfa'>hello world </Tooltip> asdasd <Tooltip placement='top' content='hello1 world1'>xyx sdfsdfsdfdsf sss</Tooltip></p>";
+  var i = 0;
+f(x);
+
+function f(x) {
+  // console.log(x)
+  let start = x.indexOf("<Tooltip");
+  let end = x.indexOf("</Tooltip>");
+  let resp = x.substring(start, end + 10);
+  //console.log("aaa "+i)
+  if (i === 0) {
+    //console.log("aaa "+i)
+    extractRemainingText(i, start);
+  }
+  //console.log(resp)
+  let content = prepareContent(resp);
+  let text = prepateText(resp);
+  arr.push({ content: content, text: text });
+  let nextText = x.substr(end + 10, x.length);
+  if (nextText.includes("<Tooltip")) {
+    f(nextText);
+    i++;
+  }
 }
-console.log(JSON.stringify(arr))
-function prepateText(resp){
- // console.log(resp)
-let start = resp.indexOf(">")
- let end = resp.lastIndexOf("<")
- //console.log(start +" "+end)
- //d =d[1].match(new RegExp("content" + "(.*)" + "</"));
- let d2 = resp.substring(start+1, end);
- //console.log(d2)
- return d2;
+console.log(JSON.stringify(arr));
+function prepateText(resp) {
+  // console.log(resp)
+  let start = resp.indexOf(">");
+  let end = resp.lastIndexOf("<");
+  //console.log(start +" "+end)
+  //d =d[1].match(new RegExp("content" + "(.*)" + "</"));
+  let d2 = resp.substring(start + 1, end);
+  //console.log(d2)
+  return d2;
 }
 
-function extractRemainingText(text){
-
+function extractRemainingText(i, j) {
+  console.log(i + " XXXX " + j);
 }
 
-function prepareContent(resp){
- let d = resp.split("Tooltip")
- let d1=d[1]
- //console.log(d1)
- let start = d1.indexOf("content=")
- let end = d1.indexOf("'>")
- //console.log(end)
- //d =d[1].match(new RegExp("content" + "(.*)" + "</"));
- let d2 = d1.substring(start+9, end);
-// console.log(d2)
-return d2;
+function prepareContent(resp) {
+  let d = resp.split("Tooltip");
+  let d1 = d[1];
+  //console.log(d1)
+  let start = d1.indexOf("content=");
+  let end = d1.indexOf("'>");
+  //console.log(end)
+  //d =d[1].match(new RegExp("content" + "(.*)" + "</"));
+  let d2 = d1.substring(start + 9, end);
+  // console.log(d2)
+  return d2;
 }
