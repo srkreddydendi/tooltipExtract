d114ea99bc275f8e1c3a49a5cefb0655e85d428b
srkreddydendi <srkreddydendi@gmail.com>
2020-10-16 21:59:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 var arr = [];
 var x =
   "<p> abcd <Tooltip placement='top' content='hello world aurn welcome'>abcd hello worlds ared </Tooltip> xyz asdfsadfsdfsdfsdfdsf <Tooltip placement='top' content='xus asdfsadfdfa'>hello world </Tooltip> asdasd <Tooltip placement='top' content='hello1 world1'>xyx sdfsdfsdfdsf sss</Tooltip>jjjj</p>";
-  var i = 0;
+var i = 0;
 f(x);
 
 function f(x) {
-  // console.log(x)
+  //console.log(x);
   let start = x.indexOf("<Tooltip");
   let end = x.indexOf("</Tooltip>");
   let resp = x.substring(start, end + 10);
+  //console.log("start " + start + " end " + end);
+  //if (i === 0) {
   //console.log("aaa "+i)
-  if (i === 0) {
-    //console.log("aaa "+i)
+  extractRemainingText(i, start, x);
+  //}
+  /*else {
     extractRemainingText(i, start, x);
-  }else{
-    extractRemainingText(i, start, x);
-  }
+  }*/
   //console.log(resp)
   let content = prepareContent(resp);
   let text = prepateText(resp);
@@ -30,10 +31,11 @@ function f(x) {
   if (nextText.includes("<Tooltip")) {
     i++;
     f(nextText);
-    
+  } else {
+    extractRemainingText(end + 10, x.length, x);
   }
 }
-console.log(JSON.stringify(arr));
+//console.log(JSON.stringify(arr));
 function prepateText(resp) {
   // console.log(resp)
   let start = resp.indexOf(">");
@@ -47,7 +49,7 @@ function prepateText(resp) {
 
 function extractRemainingText(i, j, t) {
   //console.log(i + " XXXX " + j);
-  let r = t.substring(i , j);
+  let r = t.substring(i, j);
   console.log(r);
 }
 
